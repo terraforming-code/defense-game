a2ccbf5663ebde1dce887115d81f56d5a28aa268
EAexist <megatwins@kaist.ac.kr>
2023-05-23 17:50:27 +0900

    First commit, waypoint, pooler and .gitignore

---
+++ Assets/Scripts/Waypoint/Editor/WaypointEditor.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(Waypoint)) ]
+public class WaypointEditor : Editor
+{
+    Waypoint Waypoint => target as Waypoint;
+
+    private void OnSceneGUI()
+    {
+        Handles.color = Color.cyan;
+        for(int i = 0; i < Waypoint.Points.Length; i++)
+        {
+            EditorGUI.BeginChangeCheck();
+
+            // Create Handles
+            Vector3 currentWaypointPoint = Waypoint.CurrentPosition + Waypoint.Points[i];
+            Vector3 newWaypointPoint = Handles.FreeMoveHandle(currentWaypointPoint,
+                Quaternion.identity, 0.7f,
+                new Vector3(0.3f, 0.3f, 0.3f), Handles.SphereHandleCap);
+
+            // Create text
+            GUIStyle textStyle = new GUIStyle();
+            textStyle.fontStyle = FontStyle.Bold;
+            textStyle.fontSize = 16;
+            textStyle.normal.textColor = Color.white;
+            Vector3 textAlignment = Vector3.down * 0.35f + Vector3.right * 0.35f;
+            Handles.Label(Waypoint.CurrentPosition + Waypoint.Points[i] + textAlignment,
+                $"{i + 1}", textStyle);
+            // EditorGUI.EndChangeCheck();
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(target, "Free Move Handle");
+                Waypoint.Points[i] = newWaypointPoint - Waypoint.CurrentPosition;
+            }
+        }
+    }
+}
